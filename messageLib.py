5f3e303ef620edf331363248a190003df36e3725
Angus Tomlinson <thekingsbondslave@gmail.com>
2017-03-12 20:57:44 -0600

    Added driving and autonomy logic to the client. Added a message library for sending messages. I still need to finish the logic for the actuators, bucket, and motor speed adjustment.

---
+++ messageLib.py
@@ -0,0 +1,25 @@
+import enum
+
+class forwardingPrefix(enum.Enum):
+    CLIENT = '-c'
+    TANGO = '-t'
+    MOTOR = '-m'
+    DEBUG = '-d'
+    STATUS = '-s'
+
+class subMessagePrefix(enum.Enum):
+    LEFT_MOTOR = 'l'
+    RIGHT_MOTOR = 'm'
+    ACTUATOR = 'a'
+    BUCKET = 'b'
+    SERVO = 's'
+
+class Message():
+    # Constructor expects a receiver and a submessage dictionary.
+    # Submessage dictionary format
+    def __init__(self, forwardingPrefix, subMessages):
+        subMessageString = ''
+        for subMessagePrefix, subMessage in subMessages.items():
+            subMessageString += subMessagePrefix + str(subMessage) + '|'
+
+        self.message = forwardingPrefix + subMessageString
